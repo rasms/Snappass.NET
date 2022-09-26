6745f02258d8cc824c075e779ff0b976aa585076
Rasmus Leißner <rasmus@leissner.io>
2022-09-26 10:22:38 +0200

    added pw generation

---
+++ Snappass.NET/wwwroot/js/generate_pw.js
@@ -0,0 +1,12 @@
+﻿function generatePassword() {
+    var passwordLength = 24;
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$§%-_<>&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById('password').value = password;
+}
+
+document.getElementById("generate-pw").addEventListener("click", generatePassword);
